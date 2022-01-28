3e1df2e52c32a284aacb6ef5e6526ba5f193c654
NabiiBux <nabibuxjat15tl36@gmail.com>
2022-01-28 09:22:52 -0800

    .

--- pages/Main/Section_component/Spec.jsx
+++ pages/Main/Section_component/Spec.jsx
@@ -12,16 +12,13 @@ export default function Spec() {
                         <div data-w-id="4861263c-e02e-e57e-33d7-82e204ceb29f" style={{ opacity: 1 }} className="paragraph-note">
                             <div className="note-icon"><img src="https://uploads-ssl.webflow.com/618920e5b4d12352352ee7e3/618c014ad5ca024c357e28b_notice-icon.svg" loading="lazy" alt="" /></div>
                             <div className="note-text">
-                                <div className="notice-text">Now they have come together to find a better future for the world. Together they will battle abominations and neglect that was bestowed upon the humans of the world.
-                                
+                                <div className="notice-text">Now they have come together to find a better future for the world. <br /> Together they will battle abominations and neglect that was bestowed upon the humans of the world.
                                     <br />
-            
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="samurai-grunge-holder"><img src="/img/61914e1769a98ac64d37a33f_samurai%20unge.png" loading="lazy" data-w-id="ced2d943-df49-bc80-b937-adf05d2bcff8" sizes="(max-width: 479px) 100vw, (max-width: 1279px) 50vw, 600px" srcSet="/img/61914e1769a98ac64d37a33f_samurai%20grne-p-500.png 500w, /img/61914e1769a98ac64d37a33f_samurai%20grun.png 814w" alt="" className="image-8" /></div>
             </div>
         </div>
     )
